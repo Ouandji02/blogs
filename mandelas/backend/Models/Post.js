9a7a57e35d9dab0dbc352989c2bcc7abb11a9da8
Thierry <ouandjithierry00@gmail.com>
2021-11-17 21:27:54 +0100

    change

--- mandelas/backend/Models/Post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const postSchema = mongoose.Schema({
-    nom : { type : String, require: true},
-    message : { type : String, require: true}
-})
-
-module.exports = mongoose.model('Post', postSchema)
--- mandelas/backend/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-const http = require('http')
-
-const port = 8080
-app.set('post',3000)
-const server = http.createServer(app)
-server.listen(port,()=> console.log(` Ecoute au port ${port}`))
